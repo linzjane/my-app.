00761248047f884bf08a292aca8a2ab6704d2924
linzjane <87429934+linzjane@users.noreply.github.com>
2021-10-18 18:05:29 +0100

    Amended API address

--- src/Weather.js
+++ src/Weather.js
@@ -35,7 +35,7 @@ export default function Weather(props) {
 
   function search() {
     const apiKey = "779282a2feb268a8ab73225f96fab2da";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
 
